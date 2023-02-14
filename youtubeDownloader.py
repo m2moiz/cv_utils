5e7f4528565440aca237f9665575c5e4d6da04bc
Muhammad Moiz <moiz_1995@ymail.com>
2023-02-14 15:24:44 +0100

    Created the audio and video download functions

---
+++ youtubeDownloader.py
@@ -0,0 +1,52 @@
+!pip install -U yt-dlp
+import os
+import yt_dlp as youtube_dl
+from typing import Optional
+
+class YoutubeDownloader:
+    def video_download(self, link: str, filename: Optional[str] = None, location: Optional[str] = None):
+        """
+        Downloads a YouTube video in the best quality available and converts it to MP4 format.
+
+        :param link: The YouTube video link.
+        :param filename: The name to save the video as. If not provided, the video title will be used.
+        :param location: The location to save the video. If not provided, the current working directory will be used.
+        """
+        file_path = os.path.join(location, filename) if filename else os.path.join(location, '%(title)s.%(ext)s')
+        if os.path.exists(file_path):
+            response = input(f"{file_path} already exists. Do you want to overwrite it? (y/n): ")
+            if response.lower() != 'y':
+                return
+
+        ydl_opts = {
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',
+            'outtmpl': file_path,
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+
+    def audio_download(self, link: str, filename: Optional[str] = None, location: Optional[str] = None):
+        """
+        Downloads the audio of a YouTube video in MP3 format.
+
+        :param link: The YouTube video link.
+        :param filename: The name to save the audio as. If not provided, the video title will be used.
+        :param location: The location to save the audio. If not provided, the current working directory will be used.
+        """
+        file_path = os.path.join(location, filename) if filename else os.path.join(location, '%(title)s.mp3')
+        if os.path.exists(file_path):
+            response = input(f"{file_path} already exists. Do you want to overwrite it? (y/n): ")
+            if response.lower() != 'y':
+                return
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': file_path,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }]
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
